0549da552e16c20d0f09704ec6460133b70516fa
Gabriel Medina <gmedina.santos@gmail.com>
2020-08-18 18:49:31 -0400

    Change json URL

--- src/pages/FavoritesBags/index.js
+++ src/pages/FavoritesBags/index.js
@@ -25,7 +25,7 @@ class FavoritesBags extends React.Component {
   }
 
   componentDidMount() {
-    axios.get("http://localhost:3001/bags").then((response) => {
+    axios.get("https://jsonkeeper.com/b/VFDC").then((response) => {
       let storeBags = response.data;
       storeBags = orderBy(storeBags, ["university.name", "course.name"], ["asc", "asc"]);
       
